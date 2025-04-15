3602d1d595eb7250c646c7e98dc69841eb5b93f7
sam isgrove <samis@hey.com>
2025-04-15 20:57:24 +1000

    feat: added tresitter context

---
+++ lua/custom/plugins/treesitter-context.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+      max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    },
+  },
+}
