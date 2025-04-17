423e7b782b87bb9434659b3b2b982d2a38a0b0c9
sam isgrove <samis@hey.com>
2025-04-17 22:58:13 +1000

    feat: center the screen when scrolling

--- init.lua
+++ init.lua
@@ -111,11 +111,11 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
+-- Center the screen after
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down by half a screen' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up by half a screen' })
+vim.keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'Scroll down a screen' })
+vim.keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'Scroll up a screen' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
